8be5bef7ec2083cb0852ca36649bc808f6f4da66
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-04-16 19:43:27 +0600

    Add logo

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -1,4 +1,10 @@
 import React from "react";
+import logo from "../../../assets/logo.png";
 import classes from "./Logo.module.css";
 
-export default () => <div className={classes.Logo}>Logo</div>;
+export default () => (
+  <div className={classes.Logo}>
+    <img src={logo} />
+    <span>Milfshake Builder</span>
+  </div>
+);
