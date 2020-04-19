6efced67276cc713be0e87b68a97c8815a0175c6
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-04-19 19:50:18 +0600

    Change img

--- src/components/MilkshakeBuilder/MilkshakeKit/Milk/Milk.js
+++ src/components/MilkshakeBuilder/MilkshakeKit/Milk/Milk.js
@@ -2,22 +2,7 @@ import React from "react";
 import classes from "./Milk.module.css";
 
 export default ({ type }) => {
-  const milkClasses = [classes.Milk];
-
-  switch (type) {
-    case "milk":
-      milkClasses.push(classes.milk);
-      break;
-
-    case "chocolate":
-      milkClasses.push(classes.chocolate);
-      break;
-
-    case "banana":
-    default:
-      milkClasses.push(classes.banana);
-      break;
-  }
+  const milkClasses = [classes.Milk, classes[type]];
 
   return <div className={milkClasses.join(" ")}></div>;
 };
