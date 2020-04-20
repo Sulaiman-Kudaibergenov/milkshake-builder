ea2778f40dacee4fc6ec0351970d16020074e2cb
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-04-20 09:20:27 +0600

    Nav name change to Navigation and Add NavigationItem

---
+++ src/components/UI/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Navigation.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+export default () => (
+  <ul className={classes.Navigation}>
+    <NavigationItem url="/" active>
+      Sushi Builder
+    </NavigationItem>
+    <NavigationItem url="/checkout">Checkout</NavigationItem>
+  </ul>
+);
