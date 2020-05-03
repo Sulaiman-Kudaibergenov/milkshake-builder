a8d4ba6a0bf1ced2c54126bec819fce69d169d1b
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-05-03 22:06:06 +0600

    Rename component

--- src/components/MilkshakeBuilder/MilkshakeKit/Milk/Milk.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Milk.module.css";
-
-export default ({ type }) => {
-  const milkClasses = [classes.Milk, classes[type]];
-
-  return <div className={milkClasses.join(" ")}></div>;
-};
