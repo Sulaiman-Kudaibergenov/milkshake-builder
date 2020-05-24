542fdc402bc07109f3a2c41f1ec172558b0d51d1
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-05-24 21:48:53 +0600

    Send ingredients to checkout

--- src/containers/MilkshakeBuilder/MilkshakeBuilder.js
+++ src/containers/MilkshakeBuilder/MilkshakeBuilder.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "../../axios";
 import Milkshake from "../../components/MilkshakeBuilder/Milkshake/Milkshake";
 import classes from "./MilkshakeBuilder.module.css";
 import MilkshakeControls from "../../components/MilkshakeBuilder/MilkshakeControls/MilkshakeControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/MilkshakeBuilder/OrderSummary/OrderSummary";
-import axios from "../../axios";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
@@ -23,6 +24,7 @@ export default withErrorHandler(() => {
   const [canOrder, setCanOrder] = useState(false);
   const [isOrdering, setIsOrdering] = useState(false);
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   function checkCanOrder(ingredients) {
     const total = Object.keys(ingredients).reduce((total, ingredient) => {
@@ -39,24 +41,17 @@ export default withErrorHandler(() => {
   }
 
   function finishOrder() {
-    const order = {
-      ingredients: ingredients,
-      price: price,
-      delivery: "Fast",
-      customer: {
-        name: "Sultan",
-        phone: "0500100700",
-        address: {
-          street: "Toktogula",
-          city: "Karakol",
-        },
-      },
-    };
+    const queryParams = Object.keys(ingredients).map(
+      (ingredient) =>
+        encodeURIComponent(ingredient) +
+        "=" +
+        encodeURIComponent(ingredients[ingredient])
+    );
+    queryParams.push("price=" + encodeURIComponent(price.toFixed(2)));
 
-    setLoading(true);
-    axios.post("/orders.json", order).then((response) => {
-      setLoading(false);
-      setIsOrdering(false);
+    history.push({
+      pathname: "/checkout",
+      search: queryParams.join("&"),
     });
   }
 
