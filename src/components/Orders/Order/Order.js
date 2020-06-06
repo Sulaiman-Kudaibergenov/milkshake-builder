d92b907f9e08664cbfe165edb713c352a5f8cba4
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-06-06 18:15:30 +0600

    Fix orders loading styles

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -1,19 +1,10 @@
 import React from "react";
 import classes from "./Order.module.css";
 
-const CONTROLS = {
-  chocolate: "Chocolate",
-  banana: "Banana",
-  cherry: "Cherry",
-  strawberry: "Strawberry",
-  orange: "Orange",
-  berry: "Berry",
-};
-
 export default ({ price, ingredients, details }) => {
-  const ingredientsOutput = Object.keys(ingredients).map((key) => (
-    <span key={key} className={classes.ingredient}>
-      {CONTROLS[key]} ({ingredients[key]})
+  const ingredientsOutput = Object.keys(ingredients).map((ingredient) => (
+    <span key={ingredient} className={classes.ingredient}>
+      {ingredients[ingredient].label} ({ingredients[ingredient].quantity})
     </span>
   ));
   const detailsOutput = (
