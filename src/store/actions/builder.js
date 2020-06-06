e6a6a1926432bbe03538866460775e2866348192
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-06-06 16:01:32 +0600

    Use action getters

---
+++ src/store/actions/builder.js
@@ -0,0 +1,13 @@
+import { ADD_INGREDIENT, REMOVE_INGREDIENT } from "./types";
+
+export const add = (dispatch, ingredient) =>
+  dispatch({
+    type: ADD_INGREDIENT,
+    ingredient,
+  });
+
+export const remove = (dispatch, ingredient) =>
+  dispatch({
+    type: REMOVE_INGREDIENT,
+    ingredient,
+  });
