b71df549b894c86e94214c32a87ded92cbb91a47
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-06-07 19:37:09 +0600

    Add token expiration to localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,22 @@ export const logout = (dispatch) =>
     type: AUTH_LOGOUT,
   });
 
+export const restore = (dispatch) => {
+  const idToken = localStorage.getItem("idToken");
+  const localId = localStorage.getItem("localId");
+
+  if (idToken && localId) {
+    const expirationDate = new Date(localStorage.getItem("expirationDate"));
+    if (expirationDate > new Date()) {
+      success(dispatch, { idToken, localId });
+      timeout(
+        dispatch,
+        (expirationDate.getTime() - new Date().getTime()) / 1000
+      );
+    }
+  }
+};
+
 export const timeout = (dispatch, seconds) =>
   setTimeout(() => logout(dispatch), seconds * 1000);
 
@@ -42,6 +58,11 @@ export const auth = (dispatch, method, email, password) =>
       returnSecureToken: true,
     })
     .then(({ data }) => {
+      const expirationDate = new Date(
+        new Date().getTime() + data.expiresIn * 1000
+      );
+      localStorage.setItem("expirationDate", expirationDate);
+
       localStorage.setItem("idToken", data.idToken);
       localStorage.setItem("localId", data.localId);
 
@@ -49,14 +70,3 @@ export const auth = (dispatch, method, email, password) =>
       timeout(dispatch, +data.expiresIn);
     })
     .catch((error) => fail(dispatch, error));
-
-export const restore = (dispatch) => {
-  const idToken = localStorage.getItem("idToken");
-  const localId = localStorage.getItem("localId");
-
-  if (idToken && localId) {
-    success(dispatch, { idToken, localId });
-  } else {
-    logout(dispatch);
-  }
-};
